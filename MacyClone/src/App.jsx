bf7f0c6c5c1d73e98dce532491e6c031f622d1f5
Mount-Blanc <mountblanc100@gmail.com>
2023-05-09 13:14:07 -0500

    added list items to category rail

--- MacyClone/src/App.jsx
+++ MacyClone/src/App.jsx
@@ -6,14 +6,32 @@ function App() {
   return (
     <>
      <header>
-      <section>logo
+      <section className='search-rail'>logo
         <form>
           <div>
             <input className='search-bar' placeholder='Search'/>
           </div>
         </form>
       </section>
-      <section>category rail</section>
+      <section>category rail
+        <ul>
+          <li>Women</li>
+          <li>Men</li>
+          <li>Kids</li>
+          <li>Home</li>
+          <li>Beauty</li>
+          <li>Shoes</li>
+          <li>Handbags</li>
+          <li>Jewelry</li>
+          <li>Furniture</li>
+          <li>Toys</li>
+          <li>Gifts</li>
+          <li>Trending</li>
+
+
+
+        </ul>
+      </section>
 
      </header>
     </>
